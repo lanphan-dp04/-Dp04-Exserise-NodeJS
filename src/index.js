4374b7a0b49be15352aa5b35cf927892a64a0852
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-21 09:25:32 +0700

    create resource base nodejs

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const methodOverride = require('method-override')
+const bodyParser = require("body-parser");
+const db = require("./config/db");
+const app = express()
+const port = 3000
+
+const route = require('./routes/index.route')
+app.use(express.static(path.join(__dirname, "public")));
+
+//connect db
+db.connect();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(methodOverride('_method'))
+
+//Template engine
+const hbs = handlebars.engine({
+  extname: ".hbs",
+  helpers: {
+    sum: (a,b) => a +b,
+  }
+});
+app.engine("hbs", hbs);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, 'resources','views'));
+
+//HTTP logger
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+// route innit
+route(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
