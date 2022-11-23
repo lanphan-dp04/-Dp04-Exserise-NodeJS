07d3ebdd9afcbba522031ad183001661aa549a69
NgoSinh <ngosinh0@gmail.com>
2022-11-23 13:23:44 +0700

    layout template admin

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const db = require("./config/db");
-const app = express()
-const port = 3000
+const app = express();
+const port = 5500;
 
-const route = require('./routes/index.route')
+const route = require("./routes/index.route");
 app.use(express.static(path.join(__dirname, "public")));
 
 //connect db
@@ -20,32 +20,29 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 //Template engine
 const hbs = handlebars.engine({
   extname: ".hbs",
   helpers: {
-    sum: (a,b) => a +b,
-  }
+    sum: (a, b) => a + b,
+  },
 });
 app.engine("hbs", hbs);
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, 'resources','views'));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-
-
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 // route innit
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
