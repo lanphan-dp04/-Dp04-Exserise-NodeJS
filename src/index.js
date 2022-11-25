70760156ae0907c920b426a68d122f8a748e201d
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-25 16:28:21 +0700

    fix deploy

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ app.get("/",checkAuth, (req, res) => {
 
 route(app);
 
-app.listen(5000, function(){
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
